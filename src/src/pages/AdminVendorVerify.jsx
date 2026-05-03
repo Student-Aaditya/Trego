56f9a269102e213068b6a8c1d637d8b7d521b7b4
Student-Aaditya <152685023+Student-Aaditya@users.noreply.github.com>
2026-05-03 17:20:32 +0530

    updating the ui of vendor verify page

--- src/src/pages/AdminVendorVerify.jsx
+++ src/src/pages/AdminVendorVerify.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
-const API = "http://localhost:5000/vendor";
+const API = "localhost:5000";
 
 export default function AdminVendorVerify() {
   const { user } = useAuth();
